5d4895a10ec77f47151443b4fe6d618894a22e7b
smhash78 <smhash11@gmail.com>
2024-05-30 15:42:47 +0330

    Upadted node.py

--- node.py
+++ node.py
@@ -1,4 +1,6 @@
-from typing import List, Union
+from typing import List, Union, Any
+
+import pandas as pd
 
 
 class Node:
@@ -6,13 +8,30 @@ class Node:
             self,
             selected_feature: str,
             feature_type: str,
+            children_separators: List[Any],
             children: List['Node'],
-            thresholds: Union[List[float], List[int]] = None,
+            threshold: float = None,
     ):
         self.selected_feature = selected_feature
         self.feature_type = feature_type
+        self.children_separators = children_separators
         self.children = children
-        self.thresholds = thresholds
+        self.threshold = threshold
+
+    def run(self, X: pd.DataFrame):
+        split_data = {}
+
+        if self.feature_type == 'NOM':
+            grouped = X.groupby(self.selected_feature)
+
+            for category, group in grouped:
+                split_data[category] = group
+
+        else:
+            split_data['above'] = X[X[self.selected_feature] >= self.threshold]
+            split_data['below'] = X[X[self.selected_feature] < self.threshold]
+
+        return split_data
 
 
 if __name__ == '__main__':
