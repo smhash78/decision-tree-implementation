4a9e56e8e5fa344c7d09bf5a4da945aaf0baddb8
smhash78 <smhash11@gmail.com>
2024-06-02 23:43:07 +0330

    Updated main.py. Now cleaner look with two sample datasets.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
+from time import time
+
 import pandas as pd
 
 import CONSTANTS
@@ -6,24 +7,20 @@ import CONSTANTS
 from data import Data
 from decision_tree_id3 import DecisionTreeID3
 
-if __name__ == '__main__':
 
-    # Create the dataframe
+def run_test_data_1():
     df = pd.DataFrame({
-        'A': ['F'] + ['T'] * 3,
+        'A': ['F', 'T', 'T', 'T'],
         'B': ['F', 'F', 'T', 'T'],
-        'C': ['T', 'T', 'F', 'F'],
-        'target': ['F', 'T'] * 2,
+        'C': ['F', 'T', 'T', 'F'],
+        'Y': ['F', 'T', 'F', 'T'],
     })
 
-    # Split into X and y
     X = df[['A', 'B', 'C']]
-    y = df['target']
+    y = df['Y']
 
     decision_tree = DecisionTreeID3()
-
     decision_tree.train(
-
         Data(
             X,
             y,
@@ -36,3 +33,54 @@ if __name__ == '__main__':
     )
 
     decision_tree.print_tree()
+
+
+def run_test_data_2():
+    data = [
+        ['Sunny', 'Hot', 'High', 'Light', 'No'],
+        ['Sunny', 'Hot', 'High', 'Strong', 'No'],
+        ['Overcast', 'Hot', 'High', 'Light', 'Yes'],
+        ['Rain', 'Mild', 'High', 'Light', 'Yes'],
+        ['Rain', 'Cool', 'Normal', 'Light', 'Yes'],
+        ['Rain', 'Cool', 'Normal', 'Strong', 'No'],
+        ['Overcast', 'Cool', 'Normal', 'Strong', 'Yes'],
+        ['Sunny', 'Mild', 'High', 'Light', 'No'],
+        ['Sunny', 'Cool', 'Normal', 'Light', 'Yes'],
+        ['Rain', 'Mild', 'Normal', 'Light', 'Yes'],
+        ['Sunny', 'Mild', 'Normal', 'Strong', 'Yes'],
+        ['Overcast', 'Mild', 'High', 'Strong', 'Yes'],
+        ['Overcast', 'Hot', 'Normal', 'Light', 'Yes'],
+        ['Rain', 'Mild', 'High', 'Strong', 'No']
+    ]
+
+    df = pd.DataFrame(data, columns=['Outlook', 'Temperature', 'Humidity', 'Wind', 'Play Tennis?'])
+
+    X = df[['Outlook', 'Temperature', 'Humidity', 'Wind']]
+    y = df['Play Tennis?']
+
+    decision_tree = DecisionTreeID3()
+    decision_tree.train(
+        Data(
+            X,
+            y,
+            {
+                'Outlook': CONSTANTS.NOMINAL,
+                'Temperature': CONSTANTS.NOMINAL,
+                'Humidity': CONSTANTS.NOMINAL,
+                'Wind': CONSTANTS.NOMINAL,
+            }
+        )
+    )
+
+    decision_tree.print_tree()
+
+
+if __name__ == '__main__':
+    now = time()
+    run_test_data_1()
+    print(f"It took {time() - now} seconds.")
+    print()
+
+    now = time()
+    run_test_data_2()
+    print(f"It took {time() - now} seconds.")
