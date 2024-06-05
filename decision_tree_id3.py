3330b374ed2b923573d78a8da3b94cab800a029f
smhash78 <smhash11@gmail.com>
2024-06-05 22:08:55 +0330

    Updated algorithm - Added gain ration and cleaned functions and their calling procedure

--- decision_tree_id3.py
+++ decision_tree_id3.py
@@ -8,7 +8,7 @@ from data import Data
 from node import LeafNode, Node
 
 
-def get_entropy(
+def calculate_entropy(
         data: Data,
 ) -> float:
     result = 0.0
@@ -19,29 +19,66 @@ def get_entropy(
     return result
 
 
-def get_conditional_entropy(
+def calculate_conditional_entropy(
         data: Data,
         xj: str,
         threshold: Union[int, float, None] = None,
 ) -> float:
     result = 0.0
     for _, dv in data.get_dv_portions(xj, threshold).items():
-        result += len(dv) / len(data) * get_entropy(dv)
+        result += len(dv) / len(data) * calculate_entropy(dv)
 
     return result
 
 
-def get_information_gain(
+def calculate_information_gain(
         data: Data,
         xj: str,
         threshold: Union[int, float, None] = None,
 ) -> float:
-    return get_entropy(data) - get_conditional_entropy(data, xj, threshold)
+    return calculate_entropy(data) \
+           - calculate_conditional_entropy(data, xj, threshold)
 
 
-def find_best_threshold_ig(
+def calculate_split_info(
         data: Data,
         xj: str,
+        threshold: Union[int, float, None] = None,
+) -> float:
+    result = 0.0
+    for _, dv in data.get_dv_portions(xj, threshold).items():
+        ratio = len(dv) / len(data)
+        result -= ratio * log2(ratio)
+
+    return result
+
+
+def calculate_gain_ration(
+        data: Data,
+        xj: str,
+        threshold: Union[int, float, None] = None,
+) -> float:
+    return calculate_information_gain(data, xj, threshold) \
+           - calculate_split_info(data, xj, threshold)
+
+
+def calculate_gain(
+        data: Data,
+        xj: str,
+        threshold: Union[int, float, None] = None,
+        method: str = CONSTANTS.IG,
+) -> float:
+    if method == CONSTANTS.IG:
+        return calculate_information_gain(data, xj, threshold)
+
+    elif method == CONSTANTS.GR:
+        return calculate_gain_ration(data, xj, threshold)
+
+
+def find_best_threshold_gain(
+        data: Data,
+        xj: str,
+        method: str = CONSTANTS.IG,
 ) -> Tuple[float, float]:
     unique_values = data.X[xj].unique()
     if len(unique_values) == 1:
@@ -56,7 +93,7 @@ def find_best_threshold_ig(
     best_threshold = None
 
     for threshold in thresholds:
-        ig = get_information_gain(data, xj, threshold)
+        ig = calculate_gain(data, xj, threshold, method)
         if ig > best_ig:
             best_ig = ig
             best_threshold = threshold
@@ -64,31 +101,18 @@ def find_best_threshold_ig(
     return best_threshold, best_ig
 
 
-def get_best_information_gain(
-        data: Data,
-        xj: str,
-) -> Tuple[float, Union[int, float, None]]:
-    if data.feature_types[xj] == CONSTANTS.NOMINAL:
-        return get_information_gain(data, xj), None
-
-    elif data.feature_types[xj] == CONSTANTS.NUMERIC:
-        threshold, ig = find_best_threshold_ig(data, xj)
-
-        return ig, threshold
-
-
-def get_best_gain(
+def calculate_best_gain(
         data: Data,
         xj: str,
         method: str = CONSTANTS.IG,
 ) -> Tuple[float, Union[int, float, None]]:
-    # information gain
-    if method == CONSTANTS.IG:
-        return get_best_information_gain(data, xj)
+    if data.feature_types[xj] == CONSTANTS.NOMINAL:
+        return calculate_gain(data, xj, method=method), None
 
-    # TODO gain ration
-    elif method == CONSTANTS.GR:
-        pass
+    elif data.feature_types[xj] == CONSTANTS.NUMERIC:
+        threshold, ig = find_best_threshold_gain(data, xj, method)
+
+        return ig, threshold
 
 
 def choose_best_feature(
@@ -101,7 +125,7 @@ def choose_best_feature(
         threshold = None
 
         for xj in data.get_feature_names():
-            ig, t = get_best_information_gain(data, xj)
+            ig, t = calculate_best_gain(data, xj, method)
 
             if ig > best_gain:
                 best_gain = ig
@@ -133,7 +157,9 @@ def construct_tree(
     if best_feature is None:
         return LeafNode(data.y.mode()[0])
 
-    feature_values = data.feature_types[best_feature] if threshold is None else None
+    feature_values = data.feature_types[best_feature] \
+        if threshold is None \
+        else None
 
     node = Node(
         selected_feature=best_feature,
@@ -168,13 +194,23 @@ class DecisionTreeID3:
             test_data: Data,
             evaluation_metrics: Union[List[str], None] = None,
     ):
+        if evaluation_metrics is None:
+            evaluation_metrics = [CONSTANTS.ACCURACY]
+
         true_predictions = 0
         for i, row in test_data.X.iterrows():
             prediction = self.predict(row)
             if prediction == test_data.y.iloc[i]:
                 true_predictions += 1
 
-        return {CONSTANTS.ACCURACY: true_predictions / len(test_data.y)}
+        results = {
+            CONSTANTS.ACCURACY: true_predictions / len(test_data),
+        }
+        return {
+            key: value
+            for key, value in results.items()
+            if key in evaluation_metrics
+        }
 
     def predict(self, data_point: pd.Series):
         current_node = self.tree
